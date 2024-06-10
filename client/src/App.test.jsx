1b0cc751f189483ea49d439869a17fb43623b765
vzhyhunou <vicazh@tut.by>
2024-06-10 20:10:06 +0300

    client

---
+++ client/src/App.test.jsx
@@ -0,0 +1,13 @@
+describe('App', () => {
+
+    it('should render home page', async () => {
+        //renderWithHistory()
+        //expect(await screen.findByText('Croissant')).toBeDefined()
+        //expect(document.title).toEqual('Our Bread Bakery')
+    })
+
+    it('should render login page', async () => {
+        //renderWithHistory('/login')
+        //expect(await screen.findByText('Sign in')).toBeDefined()
+    })
+})
