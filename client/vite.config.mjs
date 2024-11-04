d582cdaa6dbb870f50d321cdb96aab80c49cc9a3
vzhyhunou <vicazh@tut.by>
2024-11-04 11:59:22 +0300

    merge with base

--- client/vite.config.mjs
+++ client/vite.config.mjs
@@ -1,14 +1,13 @@
-import {createRequire} from 'node:module';
-const require = createRequire(import.meta.url);
-
-import {defineConfig} from 'vite';
+import { createRequire } from 'node:module';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
 
-// eslint-disable-next-line no-unused-vars
-const {REACT_APP_SRC, REACT_APP_BASE, ...rest} = process.env;
-
-const config = {
+const config = ({
+    REACT_APP_SRC,
+    REACT_APP_BASE,
+    BUILD_PATH
+}) => ({
     plugins: [react(), eslint()],
     server: {
         port: 3013
@@ -20,22 +19,22 @@ const config = {
         }
     },
     build: {
-        outDir: process.env.BUILD_PATH
+        outDir: BUILD_PATH
     },
-    base: process.env.REACT_APP_BASE,
+    base: REACT_APP_BASE,
     /* https://github.com/orgs/react-hook-form/discussions/3894
     ... useForm has been imported from react-hook-form/dist/index.cjs.js otherwise
     useFormContext has been imported from react-hook-form/dist/index.esm.mjs */
     resolve: {
-        alias: {
+        alias: (require => ({
             'react-hook-form': require.resolve('react-hook-form'),
             '@tanstack/react-query': require.resolve('@tanstack/react-query')
-        }
+        }))(createRequire(import.meta.url))
     }
-};
+});
 
 const srcConfig = {
-    back: {
+    back: params => (config => ({
         ...config,
         server: {
             ...config.server,
@@ -51,8 +50,12 @@ const srcConfig = {
                 }
             }
         }
-    },
+    }))(config(params)),
     fake: config
 };
 
-export default defineConfig(srcConfig[process.env.REACT_APP_SRC]);
+export default defineConfig(
+    ({mode}) => (
+        params => srcConfig[params.REACT_APP_SRC](params)
+    )(loadEnv(mode, process.cwd(), ''))
+);
