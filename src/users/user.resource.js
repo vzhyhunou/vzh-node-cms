37238dadc83cfe7ba6ea321b879f25b8f78404ed
vzhyhunou <vicazh@tut.by>
2024-11-08 23:28:03 +0300

    extend mappings service

--- src/users/user.resource.js
+++ src/users/user.resource.js
@@ -1,17 +1,19 @@
 import { Dependencies, Injectable } from '@nestjs/common';
+import { getCustomRepositoryToken } from '@nestjs/typeorm';
 
 import { USERS } from './constants';
 import { User } from './user.entity';
 import { MappingsService } from '../storage/mappings.service';
 
 @Injectable()
-@Dependencies(MappingsService)
+@Dependencies(MappingsService, getCustomRepositoryToken(User))
 export class UserResource {
-  constructor(mappingsService) {
+  constructor(mappingsService, repository) {
     mappingsService.resources.push({
       resource: USERS,
       type: User,
-      name: 'vzh.cms.model.User'
+      name: 'vzh.cms.model.User',
+      repository
     });
   }
 }
