e71bb2ac46c610326360dde2d0ae294c6cfd6db4
vzhyhunou <vicazh@tut.by>
2024-11-22 21:22:13 +0300

    charset prop

--- src/datasource/configuration.js
+++ src/datasource/configuration.js
@@ -8,6 +8,7 @@ export const config = () => ({
     database: process.env.DATASOURCE_DATABASE,
     logging: process.env.DATASOURCE_LOGGING === 'true',
     autoLoadEntities: process.env.DATASOURCE_AUTO_LOAD_ENTITIES === 'true',
-    synchronize: process.env.DATASOURCE_SYNCHRONIZE === 'true'
+    synchronize: process.env.DATASOURCE_SYNCHRONIZE === 'true',
+    charset: process.env.DATASOURCE_CHARSET
   }
 });
