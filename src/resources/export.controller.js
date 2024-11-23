cea4671959630f9a97a1b1847903ec8e3703e3d4
vzhyhunou <vicazh@tut.by>
2024-11-23 17:11:51 +0300

    protect import/export

--- src/resources/export.controller.js
+++ src/resources/export.controller.js
@@ -1,7 +1,11 @@
 import { Controller, Dependencies, Bind, Get, Query } from '@nestjs/common';
+
+import { USER_TAG } from '../users/constants';
+import { Roles } from '../auth/roles.decorator';
 import { ExportService } from './export.service';
 
 @Controller('export')
+@Roles(USER_TAG.ADMIN)
 @Dependencies(ExportService)
 export class ExportController {
   constructor(exportService) {
