fb4dd63a82fa846c6725b729d57369d76e01a579
vzhyhunou <vicazh@tut.by>
2024-12-18 00:51:09 +0300

    fix remove

--- src/storage/item.subscriber.js
+++ src/storage/item.subscriber.js
@@ -42,8 +42,8 @@ export class ItemSubscriber extends StorageSubscriber {
   }
 
   async beforeRemove(e) {
-    const { entity } = e;
-    this.save(e, await this.transform(entity));
+    const { databaseEntity } = e;
+    this.save(e, await this.transform(databaseEntity));
   }
 
   afterRemove(e) {
